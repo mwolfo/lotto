eff5ba5f5bc2dac9da178dbc003963d49b63e67c
Jesus Blanco <jesus.blanco2@wakeupcode.net>
2018-09-08 12:44:38 +0200

    create app

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="main-app">
+        <header className="app-header">          
+          <h1 className="title">Lottoland</h1>
+        </header>
+        <p>
+          Hello World!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
